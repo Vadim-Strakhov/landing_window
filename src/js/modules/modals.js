85210f6f2e67b2dff685f777e2117caf97dcbb32
Vadim-Strakhov <mars1597@gmail.com>
2023-12-31 08:40:28 +0300

    added modals

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -44,7 +44,7 @@ const modals = () => {
 
   bindModal(".popup_engineer_btn", ".popup_engineer", ".popup_engineer .popup_close");
   bindModal(".phone_link", ".popup", ".popup .popup_close");
-  // showModalByTime(".popup", 60000); //_ Функция для появления модального окна через определенное время
+  showModalByTime(".popup", 60000); //_ Функция для появления модального окна через определенное время
 };
 
 export default modals;
