10ad20407e0708d28103c8f05f4365a95980cc0c
Vadim-Strakhov <mars1597@gmail.com>
2023-12-31 08:45:04 +0300

    the tabs module is ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import "./slider";
 import modals from "./modules/modals";
+import tabs from "./modules/tabs";
 
 window.addEventListener("DOMContentLoaded", () => {
   modals();
+
+  tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active"); //_ для верхних табов
+  tabs(".decoration_slider", ".no_click", ".decoration_content > div > div", "after_click"); //_ для нижних табов
 });
